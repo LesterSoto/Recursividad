440b4bf782f7f814463b6a42b2e2a2207d0fa053
LesterSoto <joellestre@gmai.com>
2022-06-20 10:12:39 -0400

    Ejercicio 3

---
+++ src/Ejercicio_3.java
@@ -0,0 +1,9 @@
+public class Ejercicio_3 {
+    public static int suma(int vector[], int a){
+        if (a==0){
+            return vector[0];
+        }else {
+            return vector[a] + suma(vector,a-1);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(Ejercicios.factorial(5));
 
-        System.out.println(Ejercicio_2.mcd(200,200));
+        //System.out.println(Ejercicio_2.mcd(200,200));
+        int suma[]={2,3,9,7,4,1};
+        System.out.println(Ejercicio_3.suma(suma, 5));
     }
 }
